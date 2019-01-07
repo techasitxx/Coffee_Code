ff1471cbd0ce1b0a29ae560c5e897a15ffe5084a
techasitxx <techasit@om4you.com>
2019-01-07 21:11:44 +0700

    edit Web-site Coffee

---
+++ js/clean-blog.js
@@ -0,0 +1,41 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Floating label headings for the contact form
+  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+  }).on("focus", ".floating-label-form-group", function() {
+    $(this).addClass("floating-label-form-group-with-focus");
+  }).on("blur", ".floating-label-form-group", function() {
+    $(this).removeClass("floating-label-form-group-with-focus");
+  });
+
+  // Show the navbar when the page is scrolled up
+  var MQL = 992;
+
+  //primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+    var headerHeight = $('#navScroll').height();
+    $(window).on('scroll', {
+        previousTop: 0
+      },
+      function() {
+        var currentTop = $(window).scrollTop();
+        //check if user is scrolling up
+        if (currentTop < this.previousTop) {
+          //if scrolling up...
+          if (currentTop > 0 && $('#navScroll').hasClass('is-fixed')) {
+            $('#navScroll').addClass('is-visible');
+          } else {
+            $('#navScroll').removeClass('is-visible is-fixed');
+          }
+        } else if (currentTop > this.previousTop) {
+          //if scrolling down...
+          $('#navScroll').removeClass('is-visible');
+          if (currentTop > headerHeight && !$('#navScroll').hasClass('is-fixed')) $('#navScroll').addClass('is-fixed');
+        }
+        this.previousTop = currentTop;
+      });
+  }
+
+})(jQuery); // End of use strict
