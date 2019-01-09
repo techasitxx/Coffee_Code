ab1f904fc6c75f03223a3d5e8da226dc8008a2ed
techasitxx <techasit@om4you.com>
2019-01-09 22:54:36 +0700

    60%

--- app/app.js
+++ app/app.js
@@ -7,35 +7,35 @@ app.config(function($stateProvider, $urlRouterProvider){
         templateUrl: 'app/Home/index.html'
     })
     .state('next', {
-        url: '/',
+        url: '/next_page',
         templateUrl: 'app/NextPage/index.html'
     })
     .state('Menu', {
-        url: '/',
+        url: '/menu_page',
         templateUrl: 'app/Menu/index.html'
     })
     .state('page1', {
-        url: '/',
+        url: '/blag_page_1',
         templateUrl: 'app/Page/1.html'
     })
     .state('page2', {
-        url: '/',
+        url: '/blag_page_2',
         templateUrl: 'app/Page/2.html'
     })
     .state('page3', {
-        url: '/',
+        url: '/blag_page_3',
         templateUrl: 'app/Page/3.html'
     })
     .state('page4', {
-        url: '/',
+        url: '/blag_page_4',
         templateUrl: 'app/Page/4.html'
     })
     .state('page5', {
-        url: '/',
+        url: '/blag_page_5',
         templateUrl: 'app/Page/5.html'
     })
     .state('page6', {
-        url: '/',
+        url: '/blag_page_6',
         templateUrl: 'app/Page/6.html'
     })
 })
---
+++ js/api.js
@@ -0,0 +1,38 @@
+function onYouTubeIframeAPIReady() {
+    yplayer = new YT.Player("mvclip", {
+        width: "640",
+        height: "360",
+        videoId: "i_XV7YCRzKo",
+        host: 'http://www.youtube.com',
+        playerVars: {
+            controls: 0,
+            showinfo: 0,
+            modestbranding: 1,
+            wmode: "transparent"
+        },
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange
+        }
+    })
+}
+
+function onPlayerReady(e) {
+    e.target.mute(), e.target.seekTo(0), e.target.playVideo()
+}
+
+function onPlayerStateChange(e) {
+    e.data === YT.PlayerState.ENDED && e.target.seekTo(0)
+}
+
+function mainVisualResize() {
+    var e = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+        t = document.getElementById("mvclip");
+    1920 > e || (t.style.width = e + "px", t.style.height = Math.floor(e / 16 * 9) + "px")
+}
+var yplayer, ytag = document.createElement("script");
+ytag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+window.onload = firstScriptTag.parentNode.insertBefore(ytag, firstScriptTag);
+window.addEventListener("load", mainVisualResize);
+window.addEventListener("resize", mainVisualResize);
